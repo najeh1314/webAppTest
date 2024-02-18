d297789b222b587276d6afc459d260223b61ba41
Najeh <mn.najeh1314@gmail.com>
2024-02-18 21:14:10 +0100

    up last

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './log-in.component.scss'
 })
 export class LogInComponent {
-  ischecked: boolean = false;
-  
+  ischecked:boolean = false;
+
   changeCheck(event: Event) {
     const target = event.target as HTMLInputElement; // Assertion de type
     this.ischecked = target.checked;
