231d8729f4489704a366a5382b179e0813a8a628
Najeh <mn.najeh1314@gmail.com>
2024-03-11 11:14:07 +0100

    db-json-server

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -1,24 +1,30 @@
-import { Component} from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit} from "@angular/core";
+import { Http2SecureServer } from "node:http2";
 
 @Component({
     selector:'app-product',
     templateUrl:'./product.html',
     styleUrl:'./product.css'
 })
-export class Product{
+export class Product implements OnInit{
+    products: Array<any> = [];
+    private http: HttpClient;
 
-    products: Array<any> = [
-        {src:"../assets/images/pc.jpg", cardTitle:"Computer", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"},
-        {src:"../assets/images/ordinateurBureau.jpg", cardTitle:"Ordinateur de bureau", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"},
-        {src:"../assets/images/accessoires.jpg", cardTitle:"Computer", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"},
-        {src:"../assets/images/pc.jpg", cardTitle:"Computer", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"},
-        {src:"../assets/images/pc.jpg", cardTitle:"Computer", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"},
-        {src:"../assets/images/pc.jpg", cardTitle:"Computer", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"},
-        {src:"../assets/images/pc.jpg", cardTitle:"Computer", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"},
-        {src:"../assets/images/pc.jpg", cardTitle:"Computer", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"},
-        {src:"../assets/images/pc.jpg", cardTitle:"Computer", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"},
-        {src:"../assets/images/pc.jpg", cardTitle:"Computer", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"},
-        {src:"../assets/images/pc.jpg", cardTitle:"Computer", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"},
-        {src:"../assets/images/smartphone.jpg", cardTitle:"Smart Phone", cardTxt:"here you read about our product, it is a braef descreption for it. Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku", cardTxtSmall:"This is an electronic device product!"}
-    ]
+    constructor(http: HttpClient){
+        this.http = http;
+    }
+
+    ngOnInit(): void {
+        this.http.get<Array<any>>("http://localhost:8089/products")
+                .subscribe({
+                    next: data =>{
+                        this.products = data;
+                    },
+                    error: err =>{
+                        console.log(err);
+                    }     
+                })    
+    }
+
 }
