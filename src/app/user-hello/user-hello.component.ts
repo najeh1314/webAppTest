b629afa303a6a0b6d050726af387c6dd9ddd177e
Najeh <mn.najeh1314@gmail.com>
2024-03-12 11:29:04 +0100

    login component not working yet

--- src/app/user-hello/user-hello.component.ts
+++ src/app/user-hello/user-hello.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-hello',
   templateUrl: './user-hello.component.html',
   styleUrl: './user-hello.component.scss'
 })
-export class UserHelloComponent {
+export class UserHelloComponent implements OnInit{
+  private users:Array<{
+    "firstName": string, "lastName": string, "userId": string, "mdp": string
+    }> = [];
+  private http: HttpClient;
+  helloMessage: string = "Hello Mr "+ this.users[1].lastName;
+  constructor(http: HttpClient){
+    this.http = http;
+    this.helloMessage = "";
+
+  }
+  private valideAuthentification: boolean = false;
+  ngOnInit(): void {
+    this.http.get<any>("http://localhost:8089/users")
+              .subscribe({
+                next: data =>{
+                  this.users = data
+                },
+                error: err =>{
+                  console.log(err);
+                }
+              })
+
+  }
 
 }
